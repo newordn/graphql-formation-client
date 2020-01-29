9deac614ac426f4b4ae395087ab3ed88fc1c9bfd
neword <newordn@gmail.com>
2020-01-29 19:33:13 +0100

    Linking api with frontend connexion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import ApolloClient from 'apollo-boost'
+import {gql} from 'apollo-boost'
+const client = new ApolloClient({
+  uri: 'http://graphql-formation-api.herokuapp.com/'
+})
+let phone=""
+let password=""
+async function connexion(phone,mot_de_passe){
+  console.log(mot_de_passe)
+  try{
+ const result = await client.mutate({
+   variables: {
+  phone,mot_de_passe
+},
+    mutation: gql`
+    mutation connexionPrismaMutation($phone:String, $mot_de_passe:String){
+      connexionPrisma(phone:$phone,mot_de_passe:$mot_de_passe){
+      token
+      user{
+        noms
+      }
+    }
+  }
+    `
+  })
+  alert("Bienvenu "+ result.data.connexionPrisma.user.noms)
+}
+catch(e){
+  console.log(e)
+  alert("L'utilisateur n'est pas inscrit ")
+}
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form style={{backgroundColor:'white',width:400,padding:10,borderRadius:16}}>
+        <div>
+        <input onChange={e=>{phone=e.target.value}} placeholder="Votre numero de telephone"/>
+        </div>
+        <div>
+        <input  onChange={e=>{password=e.target.value}}  type="password" placeholder="Mot de passe"/>
+        </div>
+        <button onClick={()=>connexion(phone,password)} type="button" style={{backgroundColor:'blue',color:'white'}}>Connexion</button>
+      </form>   
       </header>
+      
     </div>
   );
 }
